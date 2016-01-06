3b48ae37727cbe78286d6296c7a124e1e1f3e8fd
raziff <raziff@datumcorp.com>
2016-01-06 16:34:21 +0800

    revert

--- scripts/ios/didRegisterUserNotificationSettings.js
+++ scripts/ios/didRegisterUserNotificationSettings.js
@@ -38,8 +38,8 @@ if (!rootdir)
 module.exports = function (context) {
 
     var cordova_util = context.requireCordovaModule('cordova-lib/src/cordova/util'),
-        //ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser'),
-        ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser,
+        ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser'),
+        //ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser,
         projectRoot = cordova_util.isCordova(),
         xml = cordova_util.projectConfig(projectRoot),
         cfg = new ConfigParser(xml);
